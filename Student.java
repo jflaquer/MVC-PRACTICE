f65f1037e46782ec8da6e213acf660887ef4c665
Jael Flaquer <jflaquer@udel.edu>
2019-01-25 15:16:45 -0500

    Adding Student.java as the "Model"

---
+++ Student.java
@@ -0,0 +1,22 @@
+//I'm using MVC for another project so this is just some practice to make sure that I'm familiar with MVC
+public class Student{
+    private String studentID; //Student's ID Number
+    private String name;
+
+
+    public String getRollNo(){
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo){
+        this.rollNo = rollNo;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
